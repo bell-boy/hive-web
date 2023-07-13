f2d763ddcd0c5742b344de60c33ccbba9b966b1c
Femi Bello <joshuaobello95@gmail.com>
2023-07-13 13:58:23 -0500

    Implement Basic Login Logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Login from './routes/Login';
-import CreateAccount from './routes/CreateAccount';
+
+import Root from './routes/Root.jsx';
+import { loader as rootLoader } from './routes/Root.jsx';
+
+import Login from './routes/Login.jsx';
+import { action as loginAction, loader as loginLoader } from './routes/Login.jsx';
+
+import Home from './routes/Home.jsx';
+import { loader as homeLoader } from './routes/Home';
 
 const router = createBrowserRouter([
   {
-    path: '/login',
-    element: <Login />
+		path: '/',
+		element: <Root />,
+		loader: rootLoader
   },
   {
-    path: '/createAccount',
-    element: <CreateAccount />
+		path: '/login',
+		element: <Login />,
+		action: loginAction,
+		loader: loginLoader
   },
+	{
+		path: '/home',
+		element: <Home />,
+		loader: homeLoader
+	}
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
